db04e93896954d54c574ce095edb5ba8642d5cea
Shaun TENNER <stenner@c4r21s4.wethinkcode.co.za>
2019-05-23 09:56:39 +0200

    fix norm

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,17 +6,17 @@
 /*   By: stenner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:07:02 by stenner           #+#    #+#             */
-/*   Updated: 2019/05/23 09:52:31 by stenner          ###   ########.fr       */
+/*   Updated: 2019/05/23 09:56:21 by stenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+	int					i;
 	unsigned const char *s1u;
 	unsigned const char *s2u;
 
-	s1u = (unsigned const char *)s1;       
+	s1u = (unsigned const char *)s1;
 	s2u = (unsigned const char *)s2;
 	i = 0;
 	while (s1u[i] != '\0' && s2u[i] != '\0')
